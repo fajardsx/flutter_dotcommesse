04b62f165f36989e664f6b961b42bc487595b21a
fajarpelangi <fajar@dotcomsolution.co.id>
2021-11-26 13:10:32 +0700

    [add] firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:dotcom_mart/screen/mainscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
--- lib/screen/mainscreen.dart
+++ lib/screen/mainscreen.dart
@@ -1,8 +1,10 @@
 import 'package:dotcom_mart/controller/cart_controller.dart';
 import 'package:dotcom_mart/screen/cartlistscreen.dart';
 import 'package:dotcom_mart/screen/productslist.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class MainScreen extends StatefulWidget {
   MainScreen({Key? key}) : super(key: key);
@@ -12,12 +14,65 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  late FirebaseMessaging _messaging;
   int _selectedIndex = 0;
   List<Widget> _widgetOptions = [
     SafeArea(child: ProductListScreen()),
     SafeArea(child: CartListScreen()),
     SafeArea(child: Text("Account")),
   ];
+  @override
+  void initState() {
+    super.initState();
+    registerNotification();
+    checkForInitialMessage(); // handle notif went apps full close
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {
+      print("Message diterima di background");
+      print(event.notification!.title);
+      print(event.notification!.body);
+      Get.snackbar("Notification",
+          "${event.notification!.title}\n${event.notification!.body}",
+          snackPosition: SnackPosition.TOP,
+          backgroundColor: Theme.of(context).accentColor);
+    });
+  }
+
+  registerNotification() {
+    _messaging = FirebaseMessaging.instance;
+    _messaging.getToken().then((value) => print("Token FCM : ${value}"));
+    //Open app notif
+    FirebaseMessaging.onMessage.listen((event) {
+      print("Message Diterima");
+      print(event.notification!.title);
+      print(event.notification!.body);
+      Get.snackbar("Notification",
+          "${event.notification!.title}\n${event.notification!.body}",
+          snackPosition: SnackPosition.TOP,
+          backgroundColor: Theme.of(context).accentColor);
+    });
+    //background app notif listener
+    FirebaseMessaging.onBackgroundMessage(_firebasemessagingBackgroundHandler);
+  }
+
+  Future _firebasemessagingBackgroundHandler(RemoteMessage message) async {
+    print(message.messageId);
+  }
+
+  //NOTE OPTOPNAL , handle terima notif ketika apps full close
+  checkForInitialMessage() async {
+    await Firebase.initializeApp();
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+    if (initialMessage != null) {
+      print(initialMessage.notification!.body);
+      print(initialMessage.data.toString());
+      Get.snackbar("Notification",
+          "${initialMessage.notification!.title}\n${initialMessage.notification!.body}",
+          snackPosition: SnackPosition.TOP,
+          backgroundColor: Theme.of(context).accentColor);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +100,7 @@ class _MainScreenState extends State<MainScreen> {
                       title: "Cart",
                       iconActive: Icons.shopping_bag_outlined,
                       isActive: _selectedIndex == 1,
-                      showBadge: controller.itemCount>0,
+                      showBadge: controller.itemCount > 0,
                       counter: controller.itemQtyCount,
                       onTap: () {
                         print(1);
